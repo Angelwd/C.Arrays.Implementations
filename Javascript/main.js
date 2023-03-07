a0a836ffad514d857e377e148cd7f6fa38e8b0da
Angel Castro <angelcastro.pr@gmail.com>
2023-03-07 08:56:28 -0400

    codewarsmar7

--- Javascript/main.js
+++ Javascript/main.js
@@ -387,6 +387,25 @@ function digitize(n) {
 // So the final result is an array of the digits of n in reverse order.
 --------------------
 
+// Classy Extensions
+// Classy Extensions, this kata is mainly aimed at the new JS ES6 Update introducing extends keyword.
+//  You will be preloaded with the Animal class, so you should only edit the Cat class.
+// Task
+// Your task is to complete the Cat class which Extends Animal and replace the speak method to return 
+// the cats name + meows. e.g. 'Mr Whiskers meows.'
+// The name attribute is passed with this.name (JS)
+
+
+class Cat extends Animal {
+  speak(){
+    return this.name + ' meows.'
+  }
+}
+
+// In this solution, just create a function/method that returns the this.name property with the 'meows'
+// at the end. (you can also write it this way: `${this.name} meows.`)
+--------------------------------
+
 
 
 
